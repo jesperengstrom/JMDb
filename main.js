741a047b73c72e7257f39d8cbc99bcf39a4578b8
jesperengstrom <jengstro@gmail.com>
2017-02-23 00:56:50 +0100

    toggle add box, color for grades etc

--- main.js
+++ main.js
@@ -2,6 +2,51 @@
 window.addEventListener("DOMContentLoaded", () => printModule.printMovies(movieModule.getAllMovies()));
 document.getElementById("add-submit").addEventListener("click", createMovie);
 
+Array.from(document.getElementsByClassName("toggleVisible")).forEach((el) => {
+    el.addEventListener("click", () => {
+        printModule.toggleBox();
+    });
+});
+
+
+//Add form input enters here
+
+function createMovie() {
+    let movieTitle = document.getElementById("add-title").value;
+    let movieRating = document.getElementById("add-rating").value;
+    let movieYear = document.getElementById("add-year").value;
+    let movieDirector = document.getElementById("add-director").value;
+    let movieStarring = document.getElementById("add-starring").value;
+    let movieGenre = Array.from(document.querySelectorAll(".add-genre:checked")).map((val) => { return val.value; });
+    let movieCover = document.getElementById("add-cover").value;
+    let newMovie = new Movie(movieTitle, movieRating, movieYear, movieDirector, movieStarring, movieGenre, movieCover);
+    newMovie.prepareStrings();
+    printModule.toggleBox();
+    document.getElementById("add-movie-form").reset();
+}
+
+// ...enters into constructor
+
+//CONSTRUCTOR PATTERN
+//My movies-constructor for making new movie-objects
+//and prototype function for formatting the input correctly
+function Movie(title, rating, year, director, starring, genre, cover) {
+    this.title = title;
+    this.rating = [rating];
+    this.year = year;
+    this.director = director;
+    this.starring = starring;
+    this.genre = genre;
+    this.cover = cover;
+}
+
+Movie.prototype.prepareStrings = function(movie) {
+    this.starring = this.starring.split(", ");
+    console.log(this.starring);
+    movieModule.addMovie(this);
+};
+
+//...enters into storage
 
 //MODULE PATTERN
 //MovieModule - My database of existing movies, tucked away in a module so data is safe
@@ -32,8 +77,8 @@ var movieModule = (function() {
     };
 })();
 
-//PrintModule - functions that are related to printing on screen
-//Only function printMovies needs to be public, other ones are internal
+//...where it's fetched by print-to-screen module
+//Only printing + display functions need to be public, other ones are internal
 
 var printModule = (function() {
 
@@ -43,16 +88,18 @@ var printModule = (function() {
         return rating;
     }
     //unpure helper functions
+    function setGradeColor(grade) {
+        return grade > 5 ? "goodgrade" : "badgrade";
+    }
+
     function printGenres(arr) {
         let genreCode = "";
         for (let el in arr) {
             genreCode += `<div class="genre-box">${arr[el]}</div>`;
         }
-        genreCode += `<span class="inline-link"><a href="#">Edit</a></span>`
-        console.log(genreCode);
+        genreCode += `<span class="inline-link"><a href="#">Edit genre</a></span>`
         return genreCode;
     }
-
 
     return {
         printMovies: function(movies) {
@@ -64,52 +111,22 @@ var printModule = (function() {
 
                 wrapper.innerHTML += `<div class="moviebox">
                                 <img src="${movie.cover}" class="movie-cover" alt="${movie.title}"/>
-                                <h4 class="title">${movie.title} <span class="year">(${movie.year})</span></h4>
-                                <p class="credits">Director: ${movie.director}</p>
-                                <p class="credits">Starring: ${movie.starring}</p>
+                                <h4 class="title">${movie.title} <span class="tone-down">(${movie.year})</span></h4>
+                                <p class="credits tone-down">Director: ${movie.director}</p>
+                                <p class="credits tone-down">Starring: ${movie.starring.join(", ")}</p>
                                 ${printGenres(movie.genre)}
-                                <p class="rating">Rating: ${calcRating(movie.rating)} (${movie.rating.length} votes)</p>
-
-                            </div>`
+                                <p class="credits tone-down">Rating: <span class="${setGradeColor(calcRating(movie.rating))}">${calcRating(movie.rating)}</span> (${movie.rating.length} votes)</p></div>`
             }
+        },
+        toggleBox: function() {
+            let addBox = document.getElementById("add-movie-section");
+            addBox.classList.toggle("visible");
+            addBox.classList.toggle("hidden");
         }
     };
 })();
 
+//pre-existing movie objects
 
-
-
-
-
-
-
-
-//CONSTRUCTOR PATTERN
-//My movies-constructor for making new movie-objects
-function Movie(title, rating, year = "n/a", director = "n/a", starring = "n/a", genre = "none", cover = "") {
-    this.title = title;
-    this.rating = [rating];
-    this.year = year;
-    this.director = director;
-    this.starring = starring;
-    this.genre = genre;
-    this.cover = cover;
-}
-
-function createMovie() {
-    let movieTitle = document.getElementById("add-title").value;
-    let movieRating = document.getElementById("add-rating").value;
-    let movieYear = document.getElementById("add-year").value;
-    let movieDirector = document.getElementById("add-director").value;
-    let movieStarring = document.getElementById("add-starring").value;
-    let movieGenre = Array.from(document.querySelectorAll(".add-genre:checked")).map((val) => { return val.value; });
-    let movieCover = document.getElementById("add-cover").value;
-    let newMovie = new Movie(movieTitle, movieRating, movieYear, movieDirector, movieStarring, movieGenre, movieCover);
-    movieModule.addMovie(newMovie);
-    document.getElementById("add-movie-form").reset();
-
-
-}
-
-var jurassicPark = new Movie("Jurassic Park", 5, 1993, "Steven Spielberg", ["Sam Neill", "Laura Dern"], ["Action", "Thriller", "Sci-fi"], "https://upload.wikimedia.org/wikipedia/en/e/e7/Jurassic_Park_poster.jpg");
-movieModule.addMovie(jurassicPark);
+var jurassicPark = new Movie("Jurassic Park", 5, 1993, "Steven Spielberg", "Sam Neill, Laura Dern", ["Action", "Thriller", "Sci-fi"], "https://upload.wikimedia.org/wikipedia/en/e/e7/Jurassic_Park_poster.jpg");
+jurassicPark.prepareStrings();
