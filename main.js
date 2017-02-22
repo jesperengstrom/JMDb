a6fb2893da02cf83c6c7448e07af5e35704c6d30
jesperengstrom <jengstro@gmail.com>
2017-02-22 21:15:22 +0100

    added function for printing out genres etc

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+//event handlers
+window.addEventListener("DOMContentLoaded", () => printModule.printMovies(movieModule.getAllMovies()));
+document.getElementById("add-submit").addEventListener("click", createMovie);
+
+
 //MODULE PATTERN
-//My database of existing movies, tucked away in a module so data is safe
+//MovieModule - My database of existing movies, tucked away in a module so data is safe
 //providing public "endpoints" to get all movies, get some movies (filted) and addding a movie.
 
 var movieModule = (function() {
@@ -11,8 +16,7 @@ var movieModule = (function() {
         starring: ["Jodie Foster", "Anthony Hopkins"],
         genre: ["Horror", "Thriller"],
         cover: "https://upload.wikimedia.org/wikipedia/en/8/86/The_Silence_of_the_Lambs_poster.jpg"
-
-    }];
+    }, ];
     return {
         addMovie: function(obj) {
             movieDatabase.push(obj);
@@ -23,55 +27,66 @@ var movieModule = (function() {
         },
         refreshMovies: function(movies) {
             console.log(movieDatabase);
-            return printMovies(movies);
+            return printModule.printMovies(movies);
+        }
+    };
+})();
 
+//PrintModule - functions that are related to printing on screen
+//Only function printMovies needs to be public, other ones are internal
+
+var printModule = (function() {
+
+    //some pure helper functions
+    function calcRating(arr) {
+        let rating = (arr.reduce((prev, cur) => prev + cur) / arr.length).toFixed(1);
+        return rating;
+    }
+    //unpure helper functions
+    function printGenres(arr) {
+        let genreCode = "";
+        for (let el in arr) {
+            genreCode += `<div class="genre-box">${arr[el]}</div>`;
+        }
+        genreCode += `<span class="inline-link"><a href="#">Edit</a></span>`
+        console.log(genreCode);
+        return genreCode;
+    }
+
+
+    return {
+        printMovies: function(movies) {
+            var moviesToPrint = movies;
+            var wrapper = document.getElementById("movie-wrapper");
+            wrapper.innerHTML = "";
+            for (let movie of moviesToPrint) {
+                //let filteredRating = movie.rating.filter((val) => val !== undefined); //kanske kan tas bort senare
+
+                wrapper.innerHTML += `<div class="moviebox">
+                                <img src="${movie.cover}" class="movie-cover" alt="${movie.title}"/>
+                                <h4 class="title">${movie.title} <span class="year">(${movie.year})</span></h4>
+                                <p class="credits">Director: ${movie.director}</p>
+                                <p class="credits">Starring: ${movie.starring}</p>
+                                ${printGenres(movie.genre)}
+                                <p class="rating">Rating: ${calcRating(movie.rating)} (${movie.rating.length} votes)</p>
+
+                            </div>`
+            }
         }
     };
 })();
 
 
-//event handlers
-window.addEventListener("DOMContentLoaded", () => printMovies(movieModule.getAllMovies()));
-document.getElementById("add-submit").addEventListener("click", createMovie);
 
 
-//Functions that handle screen printout
-function printMovies(movies) {
-    var moviesToPrint = movies;
-    var wrapper = document.getElementById("movie-wrapper");
-    wrapper.innerHTML = "";
-    for (let movie of moviesToPrint) {
-
-        let filteredRating = movie.rating.filter((val) => val !== undefined); //kanske kan tas bort senare
-
-        if (typeof movie.starring !== 'string') {
-            movie.starring.join(", ");
-        }
 
 
-        wrapper.innerHTML += `<div class="moviebox">
-                                <img src="${movie.cover}" class="movie-cover" alt="${movie.title}"/>
-                                <h4 class="title">${movie.title} <span class="year">(${movie.year})</span></h4>
-                                <p class="credits">Director: ${movie.director}</p>
-                                <p class="credits">Starring: ${movie.starring}</p>
-                                <p class="genres">Genre: ${movie.genre.join(" ")}
-                                <span class="inline-link"><a href="#">Edit</a></span></p>
-                                <p class="rating">Rating: ${calcRating(movie.rating)} (${filteredRating.length} votes)</p>
 
-                            </div>`
-    }
-}
-
-//pure utility functions
-function calcRating(arr) {
-    let rating = (arr.reduce((prev, cur) => prev + cur) / arr.length).toFixed(1);
-    return rating;
-}
 
 
 //CONSTRUCTOR PATTERN
 //My movies-constructor for making new movie-objects
-function Movie(title, rating, year = "n/a", director = "n/a", starring = "n/a", genre = "none", cover = "images/nocover.jpg") {
+function Movie(title, rating, year = "n/a", director = "n/a", starring = "n/a", genre = "none", cover = "") {
     this.title = title;
     this.rating = [rating];
     this.year = year;
@@ -89,7 +104,6 @@ function createMovie() {
     let movieStarring = document.getElementById("add-starring").value;
     let movieGenre = Array.from(document.querySelectorAll(".add-genre:checked")).map((val) => { return val.value; });
     let movieCover = document.getElementById("add-cover").value;
-    console.log(movieCover);
     let newMovie = new Movie(movieTitle, movieRating, movieYear, movieDirector, movieStarring, movieGenre, movieCover);
     movieModule.addMovie(newMovie);
     document.getElementById("add-movie-form").reset();
@@ -97,5 +111,5 @@ function createMovie() {
 
 }
 
-var jurassicPark = new Movie("Jurassic Park", [5], 1993, "Steven Spielberg", ["Sam Neill", "Laura Dern"], ["Action", "Thriller", "Sci-fi"], "https://upload.wikimedia.org/wikipedia/en/e/e7/Jurassic_Park_poster.jpg");
+var jurassicPark = new Movie("Jurassic Park", 5, 1993, "Steven Spielberg", ["Sam Neill", "Laura Dern"], ["Action", "Thriller", "Sci-fi"], "https://upload.wikimedia.org/wikipedia/en/e/e7/Jurassic_Park_poster.jpg");
 movieModule.addMovie(jurassicPark);
---
+++ readme.md
@@ -0,0 +1,4 @@
+Modules, separated by data and printout functions.
+
+movieModule: no need for all code to acess movie database
+printModule: no need for all code to access helper functions for printing (calc average rating, looping arrays etc)
